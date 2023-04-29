a7496a039da5a73b6e76df45bc73b666b69eb676
rashedadwan99 <rashedadwan56789@gmail.com>
2023-04-29 18:47:40 +0300

    create chat

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -31,7 +31,8 @@ const getMessages = asyncHandler(async (req, res) => {
   try {
     const messages = await Message.find({ chatId: req.params.chatId })
       .select("-chatId")
-      .populate("sender", "name picture");
+      .populate("sender", "name picture")
+      .sort("-createdAt");
     if (!messages) {
       res.status(400);
       throw new Error("error occured when getting the messages");

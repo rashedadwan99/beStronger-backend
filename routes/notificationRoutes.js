af19514348f31d7ae96b3dad60888c8c10197f9d
rashedadwan99 <rashedadwan56789@gmail.com>
2024-09-16 02:42:06 +0300

    dsds

--- routes/notificationRoutes.js
+++ routes/notificationRoutes.js
@@ -6,13 +6,14 @@ const {
   readNotification,
   deleteMobileNotification,
   deleteNoticiationFromSender,
+  sendMobileNotification,
 } = require("../controllers/notificationController");
 const auth = require("../middleware/auth");
 const router = express.Router();
 
 router.route("/").post(auth, sendNotification).get(auth, getNotifications);
 router.delete("/", auth, deleteNotification);
-router.delete("/mobile/sendNotification", auth, deleteMobileNotification);
+router.post("/mobile/sendNotification", auth, sendMobileNotification);
 router.delete("/mobile/swipe/delete", auth, deleteMobileNotification);
 router.delete("/mobile/deleteFromSender", auth, deleteNoticiationFromSender);
 router.put("/:notificationId/readNotification", auth, readNotification);

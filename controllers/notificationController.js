3db708d1b5edd32d29505864a8d69cc7759ae367
rashedadwan99 <rashedadwan56789@gmail.com>
2024-09-17 17:54:10 +0300

    dsds

--- controllers/notificationController.js
+++ controllers/notificationController.js
@@ -114,6 +114,7 @@ const sendMobileNotification = asyncHandler(async (req, res) => {
 });
 const deleteNoticiationFromSender = asyncHandler(async (req, res) => {
   try {
+    console.log(req.params.postId);
     let notification;
     if (req.params.commentId)
       notification = await Notification.deleteOne({

b3ec6364c4ceaf0220a1729bb76f39c802086cf9
rashedadwan99 <rashedadwan56789@gmail.com>
2024-09-22 20:29:59 +0300

    ff

--- server.js
+++ server.js
@@ -17,9 +17,9 @@ const server = app.listen(port, () => {
 io = socket(server, {
   pingInterval: 5000, // send a heartbeat message every 5 seconds
   pingTimeout: 15000, // consider the connection dead if no message received within 15 seconds
-  cors: {
-    origin: ["https://bestrong-client.onrender.com", "http://localhost:3000"],
-  },
+  // cors: {
+  //   origin: ["https://bestrong-client.onrender.com", "http://localhost:3000"],
+  // },
 });
 io.on("connection", (socket) => {
   socket.emit("connected");
